ec5fcdfa4b9bc848a89efb8083a861e34773451b
Praise-Atadja <106454335+Praise-Atadja@users.noreply.github.com>
2023-01-23 00:57:37 +0300

    Inherits from BaseModel

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Place class that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Place class that inherits from BaseModel """
+    name = ""
