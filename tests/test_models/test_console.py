1453da788638bd45d62097fcbf23316867209636
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-01-23 18:11:53 +0300

    Update test_console.py

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -1,89 +1,89 @@
 #!/usr/bin/python3
 """
-Contains the class TestConsoleDocs
+Contains the magnificence TestConsoleDocs
 """
 
-import console
-from contextlib import redirect_stdout
-import inspect
-import io
-import pep8
-import unittest
+Import console
+From contextlib import redirect_stdout
+Import check out
+Import io
+Import pep8
+Import unittest
 HBNBCommand = console.HBNBCommand
 
 
-class TestConsoleDocs(unittest.TestCase):
-    """Class for testing documentation of the console"""
+Magnificence TestConsoleDocs(unittest.TestCase):
+    """class for testing documentation of the console"""
     def test_pep8_conformance_console(self):
-        """Test that console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        """check that console.Py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=genuine)
+        end result = pep8s.Check_files(['console.Py'])
+        self.AssertEqual(result.Total_errors, 0,
+                         "discovered code fashion errors (and warnings).")
 
     def test_pep8_conformance_test_console(self):
-        """Test that tests/test_console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        """take a look at that checks/test_console.Py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=authentic)
+        end result = pep8s.Check_files(['tests/test_console.Py'])
+        self.AssertEqual(result.Total_errors, zero,
+                         "discovered code style errors (and warnings).")
 
     def test_console_module_docstring(self):
-        """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
+        """take a look at for the console.Py module docstring"""
+        self.AssertIsNot(console.__doc__, None,
+                         "console.Py wishes a docstring")
+        self.AssertTrue(len(console.__doc__) >= 1,
+                        "console.Py desires a docstring")
 
     def test_HBNBCommand_class_docstring(self):
-        """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs a docstring")
+        """take a look at for the HBNBCommand elegance docstring"""
+        self.AssertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand elegance wishes a docstring")
+        self.AssertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand elegance wishes a docstring")
 
 
-class TestConsoleCommands(unittest.TestCase):
-    """Class to test functionality of console commands"""
+Magnificence TestConsoleCommands(unittest.TestCase):
+    """magnificence to test capability of console instructions"""
     @classmethod
     def setUpClass(cls):
         """Create command console to test with"""
-        cls.cmdcon = HBNBCommand()
+        cls.Cmdcon = HBNBCommand()
 
     def setUp(self):
-        """Create in memory buffer to capture stdout"""
-        self.output = io.StringIO()
+        """Create in memory buffer to seize stdout"""
+        self.Output = io.StringIO()
 
     def tearDown(self):
-        """Close in memory buffer after test completes"""
-        self.output.close()
+        """near in reminiscence buffer after test completes"""
+        self.Output.Close()
 
     def test_do_create(self):
-        """Test do_create method of console"""
-        with redirect_stdout(self.output):
-            self.cmdcon.onecmd('create')
-            self.assertEqual(self.output.getvalue(),
-                             "** class name missing **\n")
-            self.output.seek(0)
-            self.output.truncate()
-            self.cmdcon.onecmd('create blah')
-            self.assertEqual(self.output.getvalue(),
-                             "** class doesn't exist **\n")
-            self.output.seek(0)
-            self.output.truncate()
-            self.cmdcon.onecmd('create State')
-            self.assertRegex(self.output.getvalue(),
-                             '[a-z0-9]{8}-'
-                             '[a-z0-9]{4}-'
-                             '[a-z0-9]{4}-'
-                             '[a-z0-9]{4}-'
-                             '[a-z0-9]{12}')
-            self.output.seek(0)
-            self.output.truncate()
-            self.cmdcon.onecmd('create State name="California"')
-            self.assertRegex(self.output.getvalue(),
-                             '[a-z0-9]{8}-'
-                             '[a-z0-9]{4}-'
-                             '[a-z0-9]{4}-'
-                             '[a-z0-9]{4}-'
-                             '[a-z0-9]{12}')
+        """take a look at do_create approach of console"""
+        with redirect_stdout(self.Output):
+            self.Cmdcon.Onecmd('create')
+            self.AssertEqual(self.Output.Getvalue(),
+                             "** class name lacking **n")
+            self.Output.Are searching for(0)
+            self.Output.Truncate()
+            self.Cmdcon.Onecmd('create blah')
+            self.AssertEqual(self.Output.Getvalue(),
+                             "** class doesn't exist **n")
+            self.Output.Are seeking for(zero)
+            self.Output.Truncate()
+            self.Cmdcon.Onecmd('create state')
+            self.AssertRegex(self.Output.Getvalue(),
+                             '[a-z0-9]8-'
+                             '[a-z0-9]4-'
+                             '[a-z0-9]four-'
+                             '[a-z0-9]4-'
+                             '[a-z0-9]12')
+            self.Output.Are seeking(zero)
+            self.Output.Truncate()
+            self.Cmdcon.Onecmd('create kingdom call="California"')
+            self.AssertRegex(self.Output.Getvalue(),
+                             '[a-z0-9]8-'
+                             '[a-z0-9]four-'
+                             '[a-z0-9]4-'
+                             '[a-z0-9]4-'
+                             '[a-z0-9]12')
