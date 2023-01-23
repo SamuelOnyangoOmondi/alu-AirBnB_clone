b9e6d8cd5f25a7def86afa15bbd3edf2681b9e87
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-01-23 20:47:57 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,32 @@
 
 from uuid import uuid4
 from datetime import datetime
+
+
+# class BaseModel:
+#     """ base class for all classes"""
+#     def __init__(self):
+#         self.id = str(uuid4())
+#         self.created_at = datetime.now()
+#         self.updated_at = datetime.now()
+
+#     def __str__(self):
+#         """readable representation"""
+#         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+#     def save(self):
+#         """updates created_at when instance changes"""
+#         self.created_at = datetime.now()
+    
+#     def to_dict(self):
+#         """ print a dictionary with instances as keys"""
+#         my_dictionary = self.__dict__.copy()
+#         my_dictionary['__class__'] = self.__class__.__name__
+#         my_dictionary['created_at'] = self.created_at.isoformat()
+#         my_dictionary['updated_at'] =  self.updated_at.isoformat()
+#         return my_dictionary
+
+
 class BaseModel:
     '''Updated new base class for all classes'''
 
@@ -41,3 +67,4 @@ class BaseModel:
         my_dictionary['created_at'] = self.created_at.isoformat()
         my_dictionary['updated_at'] =  self.updated_at.isoformat()
         return my_dictionary
+    
