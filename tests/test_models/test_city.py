dce3dd640d1a99568ce2fdbbe02ca92e5667b91a
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-01-28 23:32:32 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -6,38 +6,19 @@ import unittest
 
 from models.city import City
 
-from models.base_model import BaseModel
-
 
 class TestCity(unittest.TestCase):
     """Test cases City class."""
 
-    def test_instance(self):
-        """test instance."""
-        city = City()
-        self.assertIsInstance(city, City)
+  def setUp(self):
+        self.testCity = City()
 
     def test_is_class(self):
         """test instance."""
-        city = City()
-        self.assertEqual(str(type(city)),
-                         "<class 'models.city.City'>")
+        self.assertIsInstance(self.testCity.state_id, str)
 
     def test_is_subclass(self):
-        """test is_subclass."""
-        city = City()
-        self.assertTrue(issubclass(type(city), BaseModel))
-
-    def test_name(self):
-        """test is_subclass."""
-        city = City()
-        self.assertEqual(city.name, "")
-
-    def test_state_id(self):
-        """test is_subclass."""
-        city = City()
-        self.assertEqual(city.state_id, "")
-
+        self.assertIsInstance(self.testCity.name, str)
 
 if __name__ == "__main__":
     unittest.main()
