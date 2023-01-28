d99546917d78ef2b6e1c65994c9524dda8c8e936
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-01-28 22:34:52 +0300

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
 
 """Unittest for Amenity Class."""
-
 import unittest
-
 from models.amenity import Amenity
-
-from models.base_model import BaseModel
+from models import BaseModel
 
 
 class TestAmenity(unittest.TestCase):
     """Test cases for  Amenity class."""
 
-    def test_instance(self):
-        """test instance."""
-        amenity = Amenity()
-        self.assertIsInstance(amenity, Amenity)
+        
+        def setUp(self):
+        self.testAmenity = Amenity()
 
-    def test_is_class(self):
-        """test instance."""
-        amenity = Amenity()
-        self.assertEqual(str(type(amenity)),
-                         "<class 'models.amenity.Amenity'>")
+    def test_amenity(self):
+        self.assertTrue(issubclass(self.testAmenity.__class__, BaseModel))
 
-    def test_is_subclass(self):
-        """test is_subclass."""
-        amenity = Amenity()
-        self.assertTrue(issubclass(type(amenity), BaseModel))
+   def test_name(self):
+        self.assertIsInstance(self.testAmenity.name, str)
 
-    def test_attr(self):
-        """test is_subclass."""
-        amenity = Amenity()
-        self.assertEqual(amenity.name, "")
-        self.assertIsNotNone(amenity.id)
-
-
-if __name__ == "__main__":
+ if __name__ == "__main__":
     unittest.main()
