8b310204d99ef4e75e1e8cfa08119afd3290be81
Praise-Atadja <p.atadja@alustudent.com>
2023-02-24 13:57:54 +0300

    Revert "modify"
    This reverts commit 2cab4a9978dedf1725fb35d27947f750df20aaf0.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,10 @@
 class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances
 """
 import json
+import models
+from models.base_model import BaseModel
+
+classes = { "BaseModel": BaseModel}
 
 
 class FileStorage:   
@@ -19,40 +23,23 @@ class FileStorage:
         """returns the dictionary __objects"""
         return FileStorage.__objects
         
-    def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, 'w') as f:
-            file = {}
-            file.update(FileStorage.__objects)
-            for key, val in file.items():
-                file[key] = val.to_dict()
-            json.dump(file, f)
-
     def reload(self):
         """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists """
-        
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        
         try:
             file = {}
             with open(FileStorage.__file_path, 'r') as f:
                 file = json.load(f)
-                for key, val in file.items():
+                for key, val in file.instancess():
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass  
          
     
-   
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, 'w') as f:
+            file = {}
+            file.update(FileStorage.__objects)
+            for key, val in file.instancess():
+                file[key] = val.to_dict()
+            json.dump(file, f)
